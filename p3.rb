eafb61f3301cb60e0e5ba66837925f0a43422b5c
oozzal <theoozzal@gmail.com>
2014-09-19 16:37:51 +0545

    Optimize largest prime factor algo.

--- p3.rb
+++ p3.rb
@@ -5,22 +5,18 @@ What is the largest prime factor of the number 600851475143 ?
 """
 
 def is_prime?(n)
-  (2...n).select {|num| n % num == 0}.empty?
+  !(2...n).detect {|num| n % num == 0}
 end
 
 target = 13195
-largest_prime_factor = 0
-
-(2...target).each do |num|
-  largest_prime_factor = num if target % num == 0 && is_prime?(num)
-end
 
 num = target
 while (num -= 1) >= 2
   if target % num == 0 && is_prime?(num)
-    puts "Num: #{num}, #{target}"
+    puts "Number: #{target}, Largest Prime Factor: #{num}"
     break
   end
 end
 
-(target...2).detect {|num| target % num == 0 && is_prime?(num) }
+# (target...2).detect {|num| target % num == 0 && is_prime?(num) }
+
